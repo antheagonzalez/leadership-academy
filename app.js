46d0365c20a8e5b2ff4dec86edb3bc553c94e5ea
mkhan62 <mkhan62@hawk.iit.edu>
2018-09-25 22:22:16 -0500

    Configure Port

--- app.js
+++ app.js
@@ -16,4 +16,4 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(53000, () => console.log('Example app listening on port 53000!'));
+app.listen(process.env.PORT, () => console.log('Example app listening on port 53000!'));
